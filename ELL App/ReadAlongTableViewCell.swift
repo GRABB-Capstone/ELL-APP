89a23970a75e5f704fd8548dffcb6cf3f5893b50
Brian Carreon <bcarreon@Dinos-MacBook-Air-2.local>
2016-01-22 22:25:34 -0800

    Added read along activity

---
+++ ELL App/ReadAlongTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ReadAlongTableViewCell.swift
+//  ELL App
+//
+//  Created by Brian Carreon on 1/22/16.
+//  Copyright © 2016 Bcarreon. All rights reserved.
+//
+
+import UIKit
+
+class ReadAlongTableViewCell: UITableViewCell {
+
+    @IBOutlet var wordLabel: UILabel!
+    @IBOutlet var pageLabel: UILabel!
+    @IBOutlet var noteText: UITextView!
+}
---
+++ ELL App/ReadAlongViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ReadAlongViewController.swift
+//  ELL App
+//
+//  Created by Brian Carreon on 1/22/16.
+//  Copyright © 2016 Bcarreon. All rights reserved.
+//
+
+import UIKit
+
+class ReadAlongViewController: UIViewController {
+
+    @IBOutlet var word: UITextField!
+    @IBOutlet var page: UITextField!
+    @IBOutlet var notes: UITextField!
+    
+    var wordCollection = [String]()
+    var pageCollection = [String]()
+    var notesCollection = [String]()
+    
+    @IBAction func submitButtonAction(sender: AnyObject) {
+        self.wordCollection.append(word.text!)
+        self.pageCollection.append(page.text!)
+        self.notesCollection.append(notes.text!)
+        
+        word.text = ""
+        page.text = ""
+        notes.text = ""
+    }
+    
+    @IBAction func nextButtonAction(sender: AnyObject) {
+        performSegueWithIdentifier("readAlongContinued", sender: self)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        
+        if (segue.identifier == "readAlongContinued") {
+            var vc = segue.destinationViewController as! ReadAlongTableViewController
+            vc.words = self.wordCollection
+            vc.pages = self.pageCollection
+            vc.notes = self.notesCollection
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
