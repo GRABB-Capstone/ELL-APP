48265790f9084d5762deb64841fd8c06c4418ea0
liblabs-mac <liblabs-mac@35114Em05-121478.ad.calpoly.edu>
2016-01-25 16:03:41 -0800

    images not being pulled

--- ELL App/ImageConnectViewController.swift
+++ ELL App/ImageConnectViewController.swift
@@ -9,14 +9,17 @@
 import UIKit
 import Parse
 
-class ImageConnectViewController: UIViewController {
+class ImageConnectViewController: UIViewController, SSRadioButtonControllerDelegate {
 
     var newButton: UIButton?
     let commentBox = UITextField()
     var objectId = String()
-    var query = PFQuery(className: "Book")
+    var bookQuery = PFQuery(className: "Book")
+    var imgQuery = PFQuery(className: "Image")
     let radioButtonController = SSRadioButtonsController()
+    var bookTitle = String()
     var selectedButtons = [UIButton]()
+    var newImg: UIImage?
     var img1 = [String]()
     var img2 = [String]()
     var notes = [String]()
@@ -24,14 +27,31 @@ class ImageConnectViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         var i = 0
-        let centerX = Int(self.view.center.x)
+        //let centerX = Int(self.view.center.x)
         
         radioButtonController.delegate = self
         radioButtonController.shouldLetDeSelect = true
         
-        query.getObjectInBackgroundWithId(objectId) { (object, error) -> Void in
+        bookQuery.getObjectInBackgroundWithId(objectId) { (object, error) -> Void in
             
-            let arr = object!["words"] as! [String]
+            self.bookTitle = object!["title"] as! String
+        }
+    
+        imgQuery.findObjectsInBackgroundWithBlock { (object, error) -> Void in
+            
+            /*for img in object! {
+                if (img["book"] as! String) == "Harry Potter" {
+                    let image = img["image"] as! PFFile
+                    
+                    image.getDataInBackgroundWithBlock { (imageData: NSData, error: NSError!) -> Void in
+                        self.newImg = UIImage(data: imageData)
+                    self.makeButton(self.newImg!, buttonNum: i++)
+                    print(img["book"])
+                    }
+                }
+            }*/
+            /*
+            let arr = object!["book"] as! String
             
             for word in arr {
                 if i < 8 {
@@ -53,18 +73,20 @@ class ImageConnectViewController: UIViewController {
             self.newButton!.titleLabel!.font = UIFont.systemFontOfSize(15, weight: UIFontWeightHeavy)
             self.newButton!.backgroundColor = UIColor(red: 0.439, green: 0.608, blue: 0.867, alpha: 1)
             self.view.addSubview(self.newButton!)
+            */
         }
     }
     
-    func makeButton(word: String, buttonNum: Int) {
+    func makeButton(image: UIImage, buttonNum: Int) {
         let centerX = Int(self.view.center.x)
         
         self.newButton = UIButton()
         self.newButton!.addTarget(self, action: "pressed:", forControlEvents: UIControlEvents.TouchUpInside)
         self.newButton!.frame = CGRect(x: 0, y: 0, width: 150, height: 40)
         self.newButton!.center = CGPoint(x: centerX + 110 * ((buttonNum % 2 == 0) ? -1 : 1), y: 373 + buttonNum / 2 * 85)
-        self.newButton!.setTitle(word, forState: UIControlState.Normal)
-        self.newButton!.titleLabel!.font = UIFont.systemFontOfSize(16, weight: UIFontWeightBold)
+        //self.newButton!.setTitle(image, forState: UIControlState.Normal)
+        //self.newButton!.titleLabel!.font = UIFont.systemFontOfSize(16, weight: UIFontWeightBold)
+        self.newButton!.setBackgroundImage(image, forState: UIControlState.Normal)
         self.newButton!.selected = false
         self.newButton!.backgroundColor = UIColor.clearColor()
         self.radioButtonController.addButton(self.newButton!)
@@ -90,8 +112,8 @@ class ImageConnectViewController: UIViewController {
     func submit(sender: UIButton) {
         
         if selectedButtons.count == 2 {
-            word1.append(selectedButtons[0].currentTitle!)
-            word2.append(selectedButtons[1].currentTitle!)
+            img1.append(selectedButtons[0].currentTitle!)
+            img2.append(selectedButtons[1].currentTitle!)
             notes.append(commentBox.text!)
             
             for button in selectedButtons {
