cca349f5a42daa6ad6069d04bcda9335e9a9cac5
458 Student 5 <458student5@csc235x01.ad.calpoly.edu>
2016-01-27 15:59:26 -0800

    finished ic

---
+++ ELL App/ImageConnectTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageConnectTableViewCell.swift
+//  ELL App
+//
+//  Created by 458 Student 5 on 1/27/16.
+//  Copyright © 2016 Bcarreon. All rights reserved.
+//
+
+import UIKit
+
+class ImageConnectTableViewCell: UITableViewCell {
+
+    @IBOutlet var img1: UIImageView!
+    @IBOutlet var img2: UIImageView!
+    @IBOutlet var notes: UILabel!
+
+}
---
+++ ELL App/ImageConnectTableViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ImageConnectTableViewController.swift
+//  ELL App
+//
+//  Created by 458 Student 5 on 1/27/16.
+//  Copyright © 2016 Bcarreon. All rights reserved.
+//
+
+import UIKit
+
+class ImageConnectTableViewController: UITableViewController {
+
+    var img1 = [UIImage]()
+    var img2 = [UIImage]()
+    var notes = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return img1.count
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("imagePairCell", forIndexPath: indexPath) as! ImageConnectTableViewCell
+        
+        // Configure the cell...
+        cell.img1.image = img1[indexPath.row]
+        cell.img2.image = img2[indexPath.row]
+        cell.notes.text = notes[indexPath.row]
+        return cell
+    }
+    
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return false if you do not want the specified item to be editable.
+    return true
+    }
+    */
+    
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    if editingStyle == .Delete {
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+    }
+    */
+    
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+    
+    }
+    */
+    
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return false if you do not want the item to be re-orderable.
+    return true
+    }
+    */
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+}
